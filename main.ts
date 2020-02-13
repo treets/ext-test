9e4c4ce897e54acaae91e91f12d611824124a01e
treets <mail@andreas-theis.net>
2020-02-13 08:19:17 +0100

    test2

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-// 
+/**
+ * ...
+ */
+//% color=#dd2b49 weight=100 icon="\uf2db" block="Test"
+namespace test {
+    //% block
+    export function test(): void {
+        tools.log("test() from test extension");
+    }
+}
